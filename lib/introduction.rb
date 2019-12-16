aa67f99ae6a1697b5f18df833292492b3e6343e8
Wojciech Bojarski <bojawojt@gmail.com>
2019-12-16 11:32:18 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,5 +1,5 @@
 def introduction(name)
-	puts "Hi, my name is #{name}"
+	puts "Hi, my name is #{name}."
 end
 
 def introduction_with_language(name,language)
@@ -10,86 +10,3 @@ def introduction_with_language_optional(name,language = "Ruby")
 	puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# def introduction(name)
-#  puts "Hi, my name is #{name}."
-#end
-
-#def introduction_with_language(name, language)
-#  puts "Hi, my name is #{name} and I am learning to program in #{language}."
-#end
-
-#def introduction_with_language_optional(name, language="Ruby")
-#  puts "Hi, my name is #{name} and I am learning to program in #{language}."
-#end
